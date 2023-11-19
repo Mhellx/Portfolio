1eb28c83bfb1df428a7064da519e5809d80ee2d2
Ozy <michellpereira@outlook.com.br>
2023-11-18 21:03:42 -0300

    add contato e responsividade

---
+++ script.js
@@ -0,0 +1,37 @@
+// Mudar os links ativos da navbar de acordo com a section
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+
+    });
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // Desativar o navbar e voltar o icone quando clicar em um link da navbar ou scrollar
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+// Mudar o icone da navbar e ativar a navbar ao clicar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
